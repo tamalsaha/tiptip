1ff0efb4be8c672fa74199ccc9962385fd74df5a
Tamal Saha <tamal@appscode.com>
2021-11-30 19:44:25 -0800

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"github.com/gocarina/gocsv"
 	"google.golang.org/api/option"
 	"log"
-	"strings"
 	"time"
 
 	gdrive "gomodules.xyz/gdrive-utils"
@@ -13,7 +14,14 @@ import (
 	"google.golang.org/api/sheets/v4"
 )
 
-func main() {
+// https://docs.google.com/spreadsheets/d/1evwv2ON94R38M-Lkrw8b6dpVSkRYHUWsNOuI7X0_-zA/edit#gid=584220329
+const (
+	spreadsheetId = "10Jx3-1Ww2UQ7xNjs9-CRvJX4iIA22EDu-EsLKoHp1hc"
+	sheetName     = "NEW_SIGNUP"
+	// header        = "email"
+)
+
+func main_date() {
 	now := time.Now()
 	t2 := Timestamp{now}
 	fmt.Println(now.UTC())
@@ -25,6 +33,64 @@ func main() {
 	}
 	fmt.Println(t.String())
 	// os.Exit(1)
+}
+
+func main_add_contact() {
+	now := time.Now()
+
+	client, err := gdrive.DefaultClient(".")
+	if err != nil {
+		log.Fatalf("Unable to create client: %v", err)
+	}
+
+	srv, err := sheets.NewService(context.TODO(), option.WithHTTPClient(client))
+	if err != nil {
+		log.Fatalf("Unable to retrieve Sheets client: %v", err)
+	}
+
+	campaign := DripCampaign{
+		Steps: []DripCampaignStep{
+			{
+				WaitTime: 0,
+				Subject:  "Welcome to {{.Product}}",
+				Body:     "Hey {{.Name}}, Thanks for using {{.Product}}!",
+			},
+			{
+				WaitTime: 10 * time.Second,
+				Subject:  "How are things with {{.Product}}",
+				Body:     "Hey {{.Name}}, How are things going with {{.Product}}. If you need help, contact support@appscode.com",
+			},
+			{
+				WaitTime: 30 * time.Second,
+				Subject:  "Your trial ending soon",
+				Body:     "Hey {{.Name}}, your trial of {{.Product}} is ending soon",
+			},
+		},
+	}
+
+	type ContactData struct {
+		Name    string
+		Product string
+	}
+
+	c := Contact{
+		Email: "tamal@appscode.com",
+		Data: toJson(ContactData{
+			Name:    "Tamal Saha",
+			Product: "KubeDB",
+		}),
+	}
+	campaign.Prepare(&c, now)
+
+	w := gdrive.NewWriter(srv, spreadsheetId, sheetName)
+	err = gocsv.MarshalCSV([]*Contact{&c}, w)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	now := time.Now()
 
 	client, err := gdrive.DefaultClient(".")
 	if err != nil {
@@ -40,13 +106,119 @@ func main() {
 	const (
 		spreadsheetId = "10Jx3-1Ww2UQ7xNjs9-CRvJX4iIA22EDu-EsLKoHp1hc"
 		sheetName     = "NEW_SIGNUP"
-		header        = "Email"
+		// header        = "email"
 	)
-	emails := ListEmails(err, srv, spreadsheetId, sheetName, header)
-	fmt.Println(strings.Join(emails.List(), "\n"))
+
+	campaign := DripCampaign{
+		Steps: []DripCampaignStep{
+			{
+				WaitTime: 0,
+				Subject:  "Welcome to {{.Product}}",
+				Body:     "Hey {{.Name}}, Thanks for using {{.Product}}!",
+			},
+			{
+				WaitTime: 10 * time.Second,
+				Subject:  "How are things with {{.Product}}",
+				Body:     "Hey {{.Name}}, How are things going with {{.Product}}. If you need help, contact support@appscode.com",
+			},
+			{
+				WaitTime: 30 * time.Second,
+				Subject:  "Your trial ending soon",
+				Body:     "Hey {{.Name}}, your trial of {{.Product}} is ending soon",
+			},
+		},
+	}
+
+	type ContactData struct {
+		Name    string
+		Product string
+	}
+
+	reader, err := gdrive.NewReader(srv, spreadsheetId, sheetName, 1)
+	if err != nil {
+		panic(err)
+	}
+	var contacts []*Contact
+	err = gocsv.UnmarshalCSV(reader, &contacts)
+	if err != nil {
+		panic(err)
+	}
+	for _, c := range contacts {
+		fmt.Println(c)
+		if c.Stop {
+			continue
+		}
+		if c.Step_0_Timestamp.IsZero() || now.After(c.Step_0_Timestamp.Time) && !c.Step_0_Done {
+			processStep(srv, 0, campaign.Steps[0], *c)
+			continue
+		}
+		if c.Step_1_Timestamp.IsZero() || now.After(c.Step_1_Timestamp.Time) && !c.Step_1_Done {
+			processStep(srv, 1, campaign.Steps[1], *c)
+			continue
+		}
+		if c.Step_2_Timestamp.IsZero() || now.After(c.Step_2_Timestamp.Time) && !c.Step_2_Done {
+			processStep(srv, 2, campaign.Steps[2], *c)
+			continue
+		}
+		if c.Step_3_Timestamp.IsZero() || now.After(c.Step_3_Timestamp.Time) && !c.Step_3_Done {
+			processStep(srv, 3, campaign.Steps[3], *c)
+			continue
+		}
+		if c.Step_4_Timestamp.IsZero() || now.After(c.Step_4_Timestamp.Time) && !c.Step_4_Done {
+			processStep(srv, 4, campaign.Steps[4], *c)
+			continue
+		}
+	}
 }
 
-func ListEmails(err error, srv *sheets.Service, spreadsheetId string, sheetName string, header string) sets.String {
+func processStep(srv *sheets.Service, stepIndex int, step DripCampaignStep,  c Contact) error {
+	var data Contact
+	if err := json.Unmarshal([]byte(c.Data), &data); err != nil {
+		return err
+	}
+
+	// exec subject
+	// step.Subject
+
+	// exec body
+	// step.Body
+
+	switch stepIndex {
+	case 0:
+		c.Step_0_Done = true
+	case 1:
+		c.Step_1_Done = true
+	case 2:
+		c.Step_2_Done = true
+	case 3:
+		c.Step_3_Done = true
+	case 4:
+		c.Step_4_Done = true
+	}
+
+	w := gdrive.NewRowWriter(srv, spreadsheetId, sheetName, &gdrive.Filter{
+		Header: "email",
+		By: func(v []interface{}) (int, error) {
+			for idx, entry := range v {
+				if entry.(string) == c.Email {
+					return idx,  nil
+				}
+			}
+			return -1, fmt.Errorf("missing email %s", c.Email)
+		},
+	})
+	return gocsv.MarshalCSV([]*Contact{&c}, w)
+}
+
+func toJson(v interface{}) string {
+	data, err := json.Marshal(v)
+	if err != nil {
+		panic(err)
+	}
+	return string(data)
+}
+
+func ListEmails(srv *sheets.Service, spreadsheetId string, sheetName string, header string) sets.String {
 	reader, err := gdrive.NewColumnReader(srv, spreadsheetId, sheetName, header)
 	if err != nil {
 		panic(err)
--- types.go
+++ types.go
@@ -3,8 +3,8 @@ package main
 import "time"
 
 type Contact struct {
-	Email            string   `csv:"email"`
-	Name             string   `csv:"name"`
+	Email            string    `csv:"email"`
+	Data             string    `csv:"data"` // json format
 	Stop             bool      `csv:"stop"`
 	Step_0_Timestamp Timestamp `csv:"step_0_timestamp"`
 	Step_0_Done      bool      `csv:"step_0_done"`
@@ -35,4 +35,31 @@ func (date *Timestamp) UnmarshalCSV(csv string) (err error) {
 		date.Time, err = time.Parse("01/02/2006 15:04:05", csv)
 	}
 	return err
+}
+
+type DripCampaign struct {
+	Steps []DripCampaignStep
+}
+
+type DripCampaignStep struct {
+	WaitTime time.Duration
+	Subject  string
+	Body     string
+}
+
+func (dc DripCampaign) Prepare(c *Contact, t time.Time) {
+	for idx, step := range dc.Steps {
+		switch idx {
+		case 0:
+			c.Step_0_Timestamp = Timestamp{t.Add(step.WaitTime)}
+		case 1:
+			c.Step_1_Timestamp = Timestamp{t.Add(step.WaitTime)}
+		case 2:
+			c.Step_2_Timestamp = Timestamp{t.Add(step.WaitTime)}
+		case 3:
+			c.Step_3_Timestamp = Timestamp{t.Add(step.WaitTime)}
+		case 4:
+			c.Step_4_Timestamp = Timestamp{t.Add(step.WaitTime)}
+		}
+	}
 }
