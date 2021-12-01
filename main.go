ec70f80f506917f9efeb235bcac12ac8127e0d96
Tamal Saha <tamal@appscode.com>
2021-11-30 17:43:59 -0800

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- main.go
+++ main.go
@@ -1,4 +1,49 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"strings"
 
+	gdrive "gomodules.xyz/gdrive-utils"
+	"gomodules.xyz/sets"
+	"google.golang.org/api/sheets/v4"
+)
+
+func main() {
+	client, err := gdrive.DefaultClient(".")
+	if err != nil {
+		log.Fatalf("Unable to create client: %v", err)
+	}
+
+	srv, err := sheets.New(client)
+	if err != nil {
+		log.Fatalf("Unable to retrieve Sheets client: %v", err)
+	}
+
+	// https://docs.google.com/spreadsheets/d/1evwv2ON94R38M-Lkrw8b6dpVSkRYHUWsNOuI7X0_-zA/edit#gid=584220329
+	const (
+		spreadsheetId = "1evwv2ON94R38M-Lkrw8b6dpVSkRYHUWsNOuI7X0_-zA"
+		sheetName     = "License Issue Log"
+		header        = "Email"
+	)
+	emails := ListEmails(err, srv, spreadsheetId, sheetName, header)
+	fmt.Println(strings.Join(emails.List(), "\n"))
+}
+
+func ListEmails(err error, srv *sheets.Service, spreadsheetId string, sheetName string, header string) sets.String {
+	reader, err := gdrive.NewColumnReader(srv, spreadsheetId, sheetName, header)
+	if err != nil {
+		panic(err)
+	}
+	cols, err := reader.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	emails := sets.NewString()
+	for _, row := range cols {
+		emails.Insert(row...)
+	}
+	return emails
+}
